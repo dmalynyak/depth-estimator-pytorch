9cd4cbbd98573a0875ddead31e07084da14af04d
dmalynyak <dmytro1malynyak@gmail.com>
2026-09-05 19:49:36 +0200

    add inference arguments parsing

--- src/utils/argparse.py
+++ src/utils/argparse.py
@@ -1,5 +1,6 @@
 import torch
 import argparse
+import os
 
 def parse_device(name):
     name = name.lower()
@@ -29,3 +30,29 @@ def parse_args():
     parser.add_argument("--log_path", help="path to scv file where model validation metrics will be saved")
     
     return parser.parse_args()
+
+def parse_inference_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", default="auto", choices=["auto", "cuda", "mps", "cpu"])
+    parser.add_argument("--file_path", help="path to file")
+    parser.add_argument("--model_path", help="path to checkpoint")
+    
+    return parser.parse_args()
+
+IMAGE_EXTS = {".jpg", ".jpeg", ".png"}
+VIDEO_EXTS = {".mov", ".mp4", ".avi", ".mkv", ".m4v", ".webm"}
+
+def parse_extension(name):
+    extension = os.path.splitext(name)[1].lower()
+    base = os.path.splitext(name)[0]
+
+    if extension in IMAGE_EXTS:
+        type = "image"
+        out_path = f"{base}_out.png"
+        return type, out_path
+    elif extension in VIDEO_EXTS:
+        type = "video"
+        out_path = f"{base}_out.mp4"
+        return type, out_path
+    else:
+        print(f"Unsupported file type. Supported: {IMAGE_EXTS}, {VIDEO_EXTS}")
