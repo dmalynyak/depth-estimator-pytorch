5bf26ea6f1cdee896feabc26ce697edb11ef3349
dmalynyak <dmytro1malynyak@gmail.com>
2026-09-05 21:13:09 +0200

    update __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,14 @@
 from .dataloaders import DepthNYUDataset
 from .models import NYUmodel
-
+from .losses import NYULoss
+from .metrics import get_metrics
+from .utils import Logger
+from .engine import Trainer
+from .inference_engine import Inference
 
 __all__ = [
     "DepthNYUDataset",
     "NYUmodel",
+    "get_metrics",
+    "Logger"
 ]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,11 @@
-from .visualize import visualize_1chw
+from .visualize import visualize_1chw, draw_prediction
 from .geometry import upsample_x2
+from .logging import Logger
+from .argparse import parse_args, parse_device, parse_extension, parse_inference_args
 
 
 __all__ = [
     "visualize_1chw",
     "upsample_x2",
+    "Logger",
 ]
