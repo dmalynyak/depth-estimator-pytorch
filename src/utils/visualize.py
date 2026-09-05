cf8fb58902102fead89369626ba88ab2c7d5716a
dmalynyak <dmytro1malynyak@gmail.com>
2026-09-05 19:51:59 +0200

    add inference prediction visualization

--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -52,3 +52,29 @@ def visualize_1chw(rgb, depth, depth_pred=None):
         plt.colorbar(im, ax=ax[2], fraction=0.046)
 
     plt.show()
+
+def draw_prediction(rgb, depth_pred, save_path):
+
+    rgb, depth_pred = src.dataloaders.denormalize_image_net(rgb, depth_pred)
+    rgb = rgb.squeeze(0)
+    #depth_pred = depth_pred.squeeze(0)
+    print(f"rgb: {rgb.shape}")
+    print(f"depth: {depth_pred.shape}")
+
+    rgb_plot = rgb.permute(1, 2, 0).detach().cpu().numpy()
+    depth_plot = depth_pred.permute(1, 2, 0).detach().cpu().numpy()
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
+    ax[0].imshow(rgb_plot)
+    ax[0].set_title("RGB")
+    ax[0].axis("off")
+
+    im = ax[1].imshow(depth_plot, cmap="plasma", vmin=0, vmax=10)
+    ax[1].set_title("Depth Prediction")
+    ax[1].axis("off")
+    plt.colorbar(im, ax=ax[1], fraction=0.046)
+
+    plt.savefig(save_path, bbox_inches='tight', dpi=300)
+
+    plt.close(fig)
