ea29a8deb01c0803fdebc5bc8f61a3bfcf021754
dmalynyak <dmytro1malynyak@gmail.com>
2026-09-10 22:52:59 +0200

    fix typo with channels sizes

--- src/models/kitti_depthnet.py
+++ src/models/kitti_depthnet.py
@@ -10,7 +10,7 @@ class KITTIdepthNET(nn.Module):
         # input: (B, 3, 192, 640)
         self.encoder = Resnet18Encoder()
         self.decoder = KITTIdecoder()
-        # output: [B, 16, 192, 640][B, 32, 96, 320][B, 64, 48, 160][B, 128, 24, 80]
+        # output: [B, 1, 192, 640][B, 1, 96, 320][B, 1, 48, 160][B, 1, 24, 80]
 
     def forward(self, img):
         feats = self.encoder(img)
