f8da0fd3030857c5dcc079da0a44028d43901783
dmalynyak <dmytro1malynyak@gmail.com>
2026-09-22 01:33:02 +0200

    add disparity output clarification

--- src/models/kitti_depthnet.py
+++ src/models/kitti_depthnet.py
@@ -11,6 +11,10 @@ class KITTIdepthNET(nn.Module):
         self.encoder = Resnet18Encoder()
         self.decoder = KITTIdecoder()
         # output: [B, 1, 192, 640][B, 1, 96, 320][B, 1, 48, 160][B, 1, 24, 80]
+        # output is not depth but normilized disparity = (disp − min_disp) / (max_disp − min_disp), where disp = 1/depth
+        # conversion to depth: 
+        #   scaled = min_disp + (max_disp − min_disp) · sigmoid_output(normilized disparity)
+        #   depth  = 1 / scaled
 
     def forward(self, img):
         feats = self.encoder(img)
